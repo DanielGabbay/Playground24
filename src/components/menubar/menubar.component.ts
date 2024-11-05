33811898adfdcec96a9ec2ed1a0b2846c2201532
Daniel <daniel.g@sps.co.il>
2024-11-05 17:34:30 +0200

    - add new list (finished)

--- src/components/menubar/menubar.component.ts
+++ src/components/menubar/menubar.component.ts
@@ -1,9 +1,15 @@
 import { NgClass } from '@angular/common';
-import { Component, ChangeDetectionStrategy, } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  computed,
+} from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Badge, BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { TodoListService } from '../../features/todo-list/data/todo-list.service';
 
 @Component({
   selector: 'app-menubar',
@@ -11,32 +17,57 @@ import { MenubarModule } from 'primeng/menubar';
   imports: [MenubarModule, BadgeModule, NgClass],
   templateUrl: './menubar.component.html',
   styleUrl: './menubar.component.scss',
- changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MenubarComponent {
-  protected items: MenuItem[] = [
-    {
-      label: 'מונה',
-      icon: "pi pi-stopwatch text-blue-500",
-      styleClass: "text-lg font-semibold"
-    },
-    {
-      label: 'רשימות',
-      icon: "pi pi-list-check text-blue-500",
-      styleClass: "text-lg font-semibold",
-      items: [
-        {
-          label: 'רשימה 1',
-        },
-        {
-          label: 'רשימה 2',
-        },
-        {
-          label: 'רשימה 3',
-        },
-      ],
-    },
-  ];
+  private readonly todoListService: TodoListService = inject(TodoListService);
 
-  
+  // protected items: MenuItem[] = [
+  // {
+  //   label: 'מונה',
+  //   icon: "pi pi-stopwatch text-blue-500",
+  //   styleClass: "text-lg font-semibold"
+  // },
+  // {
+  //   label: 'רשימות',
+  //   icon: "pi pi-list-check text-blue-500",
+  //   styleClass: "text-lg font-semibold",
+  //   items: [
+  //     {
+  //       label: 'רשימה 1',
+  //     },
+  //     {
+  //       label: 'רשימה 2',
+  //     },
+  //     {
+  //       label: 'רשימה 3',
+  //     },
+  //   ],
+  // },
+  // ];
+
+  protected readonly items = computed<MenuItem[]>(() => {
+    const availableListsMenuItems =
+      this.todoListService.allLists$()?.map((l) => {
+        return {
+          label: l.name,
+        } as MenuItem;
+      }) || [];
+
+    return [
+      {
+        label: 'מונה',
+        icon: 'pi pi-stopwatch text-blue-500',
+        styleClass: 'text-lg font-semibold',
+      },
+      {
+        label: 'רשימות',
+        icon: 'pi pi-list-check text-blue-500',
+        styleClass: 'text-lg font-semibold',
+        items: availableListsMenuItems,
+      },
+    ];
+  });
+
+  constructor() {}
 }
--- src/features/todo-list/data/todo-list.service.ts
+++ src/features/todo-list/data/todo-list.service.ts
@@ -1,5 +1,5 @@
-import { Inject, Injectable } from '@angular/core';
-import { Observable, of, timeout } from 'rxjs';
+import { Inject, Injectable, signal } from '@angular/core';
+import { map, Observable, of, switchMap, tap, timeout } from 'rxjs';
 import { IList } from './list.model';
 
 @Injectable({
@@ -7,17 +7,29 @@ import { IList } from './list.model';
 })
 export class TodoListService {
   private static LIST_PREFIX: string = 'LIST_';
+  private readonly _allLists$ = signal<IList[]>(null);
+  public readonly allLists$ = this._allLists$.asReadonly();
 
-  constructor() {}
+  constructor() {
+    this.getLists().subscribe();
+  }
 
-  public getLists(): Observable<IList[]> {
+  public getLists(force: boolean = false): Observable<IList[]> {
+    const allLists = this._allLists$();
+    if (!force && allLists !== null) return of(allLists);
+
     const lists = Object.keys(localStorage).flatMap((k) => {
       if (!k.startsWith(TodoListService.LIST_PREFIX)) return [];
       const listString = localStorage.getItem(k);
       const list: IList = JSON.parse(listString);
       return list;
     });
-    return of(lists).pipe(timeout(1000));
+    return of(lists).pipe(
+      timeout(1000),
+      tap((lists) => {
+        this._allLists$.set(lists);
+      })
+    );
   }
 
   public hasList(listName: string): boolean {
@@ -36,6 +48,12 @@ export class TodoListService {
       return of(false).pipe(timeout(1000));
     }
     localStorage.setItem(listID, JSON.stringify(list));
-    return of(true).pipe(timeout(1000));
+    return of(true).pipe(
+      timeout(1000),
+      switchMap(() => {
+        return this.getLists(true)
+      }),
+      map(Boolean)
+    );
   }
 }
