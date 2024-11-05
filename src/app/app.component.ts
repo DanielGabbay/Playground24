5c2f2368050649753a021dbfec166aa59e3387bd
Daniel <daniel.g@sps.co.il>
2024-11-05 09:45:09 +0200

    added primeng componets libray and other helpers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CounterComponent } from "../components/counter/counter.component";
+import { CounterComponent } from '../components/counter/counter.component';
+import { TodoCounterComponent } from '../features/todo-list/components/todo-counter/todo-counter.component';
+import { TodoListComponent } from '../features/todo-list/components/todo-list/todo-list.component';
+import { PrimeNGConfig } from 'primeng/api';
+import { Lara } from 'primeng/themes/lara';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [
+    RouterOutlet,
+    CounterComponent,
+    TodoCounterComponent,
+    TodoListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Playground24';
+  /* ------------------------------------------  Types and Constants ------------------------------------------ */
+
+  /* ------------------------------------------ PROVIDERS / SERVICES ------------------------------------------ */
+  private readonly config: PrimeNGConfig = inject(PrimeNGConfig);
+
+  /* ------------------------------------------------  Inputs ------------------------------------------------ */
+
+  /* ------------------------------------------------  Outputs ------------------------------------------------ */
+
+  /* ------------------------------------------------  Signals ------------------------------------------------ */
+
+  /* -------------------------------------------------- Data -------------------------------------------------- */
+
+  /* ------------------------------------------------  Constructor ------------------------------------------------ */
+  constructor() {
+    this.config.theme.set({preset: Lara})
+  }
+
+  /* ----------------------------------------------- Lifecycle Hooks ----------------------------------------------- */
+
+  /* ------------------------------------------------  Methods ------------------------------------------------ */
 }
